a221e3821a009913f355261832d6a060d5ea5d9b
LovelightCross <32072875+LovelightCross@users.noreply.github.com>
2017-11-16 16:03:20 -0600

    added auth mw and the login route

---
+++ src/middleware/auth.mw.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
+import { isNil } from 'ramda';
+import { comparePassword } from './bcrypt.mw';
+const expires = 1000 * 60 * 60;
+const usersPath = path.join(__dirname, '..', 'users.json');
+
+
+export const login = (req: Request, res: Response, next: NextFunction) => {
+    const { user, password } = req.body;
+    const users = JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
+    
+    const found: models.IUser = users.find((u: models.IUser): boolean => {
+        if (u.user === user) {
+            return true;
+        }
+
+        return false;
+    });
+
+    if (isNil(found)) {
+        res.sendStatus(404);
+        return;
+    }
+
+};
